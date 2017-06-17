90f91866cc19b8d2cc022cb4a4364181154caba1
Rafael Marlon Pereira Costa Baeta <rafael.baeta@gmail.com>
2017-06-17 16:22:33 -0300

    teste

--- mini_batch_run_6x3.sh
+++ mini_batch_run_6x3.sh
@@ -2,13 +2,13 @@
 
 #dataPath, outputPath(for model, images, etc), trainInstances, validationInstances,testInstances,cropSize,learningRate,weightDecay,batchSize,nIter,epochs,trainModel,instance,useMinibatch,useValidation
 
-python mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 0 10 1 1 1 1
-python mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 1 10 1 1 1 1
-python mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 2 10 1 1 1 1
-python mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 3 10 1 1 1 1
-python mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 4 10 1 1 1 1
-python mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 5 10 1 1 1 1
-python mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 6 10 1 1 1 1
-python mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 7 10 1 1 1 1
-python mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 8 10 1 1 1 1
-python mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 9 10 1 1 1 1
+python /media/tensorflow/coffee/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 0 10 1 1 1 1
+python /media/tensorflow/coffee/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 1 10 1 1 1 1
+python /media/tensorflow/coffee/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 2 10 1 1 1 1
+python /media/tensorflow/coffee/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 3 10 1 1 1 1
+python /media/tensorflow/coffee/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 4 10 1 1 1 1
+python /media/tensorflow/coffee/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 5 10 1 1 1 1
+python /media/tensorflow/coffee/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 6 10 1 1 1 1
+python /media/tensorflow/coffee/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 7 10 1 1 1 1
+python /media/tensorflow/coffee/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 8 10 1 1 1 1
+python /media/tensorflow/coffee/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_5,9_6,7_7,7_6,7_5 8_7 8_6 33 0.01 0.005 200 9 10 1 1 1 1
