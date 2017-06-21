1e285cafba2965092a926068cf0a4808e1c1f642
Baeta18 <rafael.baeta@gmail.com>
2017-06-21 15:29:42 -0300

    whatevs

--- mini_batch_subimages_6x3.py
+++ mini_batch_subimages_6x3.py
@@ -826,7 +826,7 @@ def convNet_ICPR_57(x, dropout, is_training, cropSize, weightDecay):
 	print(pool3.get_shape())
 
 
-	conv4 = _conv_layer(pool3, [4,4,256,312], 'ft_conv4', weightDecay, is_training, pad='VALID')
+	conv4 = _conv_layer(pool3, [3,3,256,312], 'ft_conv4', weightDecay, is_training, pad='VALID')
 	print("conv shape")
 	print(conv4.get_shape())
 
