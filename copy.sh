67bbf2bccc96c9357a0c0f9c61617da45fe79526
Baeta18 <rafael.baeta@gmail.com>
2017-06-21 14:35:51 -0300

    whatevs

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp -r rafaelbaeta@glcm.vpr.dcc.ufmg.br:/mnt/hdd/baeta/tensorflow/coffee/output/results/maps/4_maps_6x3_33 /home/baeta/Documentos/patreo/datasets/montesanto
+scp -r rafaelbaeta@glcm.vpr.dcc.ufmg.br:/mnt/hdd/baeta/tensorflow/coffee/output/results/maps/4_maps_6x3_9 /home/baeta/Documentos/patreo/datasets/montesanto
 
 #scp -r rafaelbaeta@glcm.vpr.dcc.ufmg.br:/mnt/hdd/baeta/tensorflow/coffee/output/results/maps/4_maps_6x3_45/test/9_5-8_6-8_7 /home/baeta/Documentos/patreo/datasets/montesanto
