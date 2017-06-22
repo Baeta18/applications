8425f3e501847761b0b43210d7f9ddf9357562ff
Baeta18 <rafael.baeta@gmail.com>
2017-06-22 17:45:51 -0300

    whatevs

--- mini_batch_subimages_6x3.py
+++ mini_batch_subimages_6x3.py
@@ -636,7 +636,7 @@ def convNet_ICPR_17(x, dropout, is_training, cropSize, weightDecay):
 
 	return fc2, logits
 
-def convNet_ICPR_33(x, dropout, is_training, cropSize, weightDecay):
+def convNet_ICPR_33_3blocks(x, dropout, is_training, cropSize, weightDecay):
 	# Reshape input picture
 	x = tf.reshape(x, shape=[-1, cropSize, cropSize, 3]) ## default: 25x25
 	#print x.get_shape()
@@ -688,6 +688,77 @@ def convNet_ICPR_33(x, dropout, is_training, cropSize, weightDecay):
 		logits = tf.add(tf.matmul(fc2, weights), biases, name=scope.name)
 
 	return fc2, logits
+
+
+def convNet_ICPR_33_4blocks(x, dropout, is_training, cropSize, weightDecay):
+	# Reshape input picture
+	x = tf.reshape(x, shape=[-1, cropSize, cropSize, 3]) ## default: 25x25
+	#print x.get_shape()
+	
+	conv1 = _conv_layer(x, [3,3,3,64], 'ft_conv1', weightDecay, is_training, pad='VALID')
+	pool1 = _max_pool(conv1, kernel=[1, 2, 2, 1], strides=[1, 2, 2, 1], name='ft_pool1', pad='VALID')
+	print("Conv")
+	print(conv1.get_shape())
+	print("Pool")
+	print(pool1.get_shape())
+
+	
+	conv2 = _conv_layer(pool1, [3,3,64,128], 'ft_conv2', weightDecay, is_training, pad='VALID')
+	pool2 = _max_pool(conv2, kernel=[1, 2, 2, 1], strides=[1, 2, 2, 1], name='ft_pool2', pad='VALID')
+	print("Conv")
+	print(conv2.get_shape())
+	print("Pool")
+	print(pool2.get_shape())
+
+	
+	conv3 = _conv_layer(pool2, [3,3,128,256], 'ft_conv3', weightDecay, is_training, pad='VALID')
+	pool3 = _max_pool(conv3, kernel=[1, 2, 2, 1], strides=[1, 2, 2, 1], name='ft_pool3', pad='VALID')
+	print("Conv")
+	print(conv3.get_shape())
+	print("Pool")
+	print(pool3.get_shape())
+
+	
+	conv2 = _conv_layer(pool1, [3,3,64,128], 'ft_conv2', weightDecay, is_training, pad='VALID')
+	pool2 = _max_pool(conv2, kernel=[1, 2, 2, 1], strides=[1, 2, 2, 1], name='ft_pool2', pad='VALID')
+	print("Conv")
+	print(conv2.get_shape())
+	print("Pool")
+	print(pool2.get_shape())
+
+	
+	conv3 = _conv_layer(pool2, [3,3,128,256], 'ft_conv3', weightDecay, is_training, pad='VALID')
+	pool3 = _max_pool(conv3, kernel=[1, 2, 2, 1], strides=[1, 2, 2, 1], name='ft_pool3', pad='VALID')
+	print("Conv")
+	print(conv3.get_shape())
+	print("Pool")
+	print(pool3.get_shape())
+
+
+	with tf.variable_scope('ft_fc1') as scope:
+		reshape = tf.reshape(pool3, [-1, 1*1*256])
+		weights = _variable_with_weight_decay('weights', shape=[1*1*256, 1024], ini=tf.contrib.layers.xavier_initializer(dtype=tf.float32), wd=weightDecay)
+		biases = _variable_on_cpu('biases', [1024], tf.constant_initializer(0.1))
+		drop_fc1 = tf.nn.dropout(reshape, dropout)
+		fc1 = tf.nn.relu(_batch_norm(tf.add(tf.matmul(drop_fc1, weights), biases), is_training, scope=scope.name))
+	
+	# Fully connected layer 2
+	with tf.variable_scope('ft_fc2') as scope:
+		weights = _variable_with_weight_decay('weights', shape=[1024, 1024], ini=tf.contrib.layers.xavier_initializer(dtype=tf.float32), wd=weightDecay)
+		biases = _variable_on_cpu('biases', [1024], tf.constant_initializer(0.1))
+
+		# Apply Dropout
+		drop_fc2 = tf.nn.dropout(fc1, dropout)
+		fc2 = tf.nn.relu(_batch_norm(tf.add(tf.matmul(drop_fc2, weights), biases), is_training, scope=scope.name))
+
+	# Output, class prediction
+	with tf.variable_scope('ft_fc3_logits') as scope:
+		weights = _variable_with_weight_decay('weights', [1024, NUM_CLASSES], ini=tf.contrib.layers.xavier_initializer(dtype=tf.float32), wd=weightDecay)
+		biases = _variable_on_cpu('biases', [NUM_CLASSES], tf.constant_initializer(0.1))
+		logits = tf.add(tf.matmul(fc2, weights), biases, name=scope.name)
+
+	return fc2, logits
+
 
 
 def convNet_ICPR_25(x, dropout, is_training, cropSize, weightDecay):
@@ -1012,7 +1083,10 @@ def convNet_ICPR(x, keep_prob, is_training, cropSize, weightDecay,blocks):
 	elif(cropSize == 17):
 		 _,logits = convNet_ICPR_17(x, keep_prob, is_training, cropSize, weightDecay)
 	elif(cropSize == 33):
-		 _,logits = convNet_ICPR_33(x, keep_prob, is_training, cropSize, weightDecay)
+		if blocks == 3:
+			_,logits = convNet_ICPR_33_3blocks(x, keep_prob, is_training, cropSize, weightDecay)
+		else:
+			_,logits = convNet_ICPR_33_4blocks(x, keep_prob, is_training, cropSize, weightDecay)
 	elif(cropSize == 25):
 		 _,logits = convNet_ICPR_25(x, keep_prob, is_training, cropSize, weightDecay)
 	elif(cropSize == 41):
