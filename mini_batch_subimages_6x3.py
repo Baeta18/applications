4f383d49dd7171d0e5c2276f5e92039b5d7909a4
Baeta18 <rafael.baeta@gmail.com>
2017-06-26 10:15:34 -0300

    whatevs

--- mini_batch_subimages_6x3.py
+++ mini_batch_subimages_6x3.py
@@ -797,7 +797,7 @@ def convNet_ICPR_41_4blocks(x, dropout, is_training, cropSize, weightDecay):
 	x = tf.reshape(x, shape=[-1, cropSize, cropSize, 3]) ## default: 25x25
 	#print x.get_shape()
 	
-	conv1 = _conv_layer(x, [5,5,3,128], 'ft_conv1', weightDecay, is_training, pad='VALID')
+	conv1 = _conv_layer(x, [5,5,3,128], 'ft_conv1', weightDecay, is_training, pad='SAME')
 	print("conv shape")
 	print(conv1.get_shape())
 
@@ -805,7 +805,7 @@ def convNet_ICPR_41_4blocks(x, dropout, is_training, cropSize, weightDecay):
 	print("pool shape")	
 	print(pool1.get_shape())
 
-	conv2 = _conv_layer(pool1, [4,4,128,192], 'ft_conv2', weightDecay, is_training, pad='VALID')
+	conv2 = _conv_layer(pool1, [4,4,128,192], 'ft_conv2', weightDecay, is_training, pad='SAME')
 	print("conv shape")
 	print(conv2.get_shape())
 
@@ -814,7 +814,7 @@ def convNet_ICPR_41_4blocks(x, dropout, is_training, cropSize, weightDecay):
 	print(pool2.get_shape())
 	
 	
-	conv3 = _conv_layer(pool2, [3,3,192,256], 'ft_conv3', weightDecay, is_training, pad='VALID')
+	conv3 = _conv_layer(pool2, [3,3,192,256], 'ft_conv3', weightDecay, is_training, pad='SAME')
 	print("conv shape")
 	print(conv3.get_shape())
 
@@ -824,7 +824,7 @@ def convNet_ICPR_41_4blocks(x, dropout, is_training, cropSize, weightDecay):
 	print(pool3.get_shape())
 
 	
-	conv4 = _conv_layer(pool3, [3,3,256,312], 'ft_conv4', weightDecay, is_training, pad='VALID')
+	conv4 = _conv_layer(pool3, [3,3,256,312], 'ft_conv4', weightDecay, is_training, pad='SAME')
 	print("conv shape")
 	print(conv4.get_shape())
 
