03a970cdbaaa0fa387a9dd519cbb7e6cd414b0d6
Baeta18 <rafael.baeta@gmail.com>
2017-06-28 15:30:19 -0300

    whatevs

--- mnist-filter-save.py
+++ mnist-filter-save.py
@@ -121,7 +121,7 @@ def loadImages(dataPath, instances, cropSize,type):
 def plotNNFilter(units,layer_number,label):
 	filters = units.shape[3]
 	plt.figure(1, figsize=(20,20))
-	gs1 = gridspec.GridSpec(4, 4)
+	gs1 = gridspec.GridSpec(20, 20)
 	gs1.update(wspace=0.025, hspace=0.05) # set the spacing between axes. 
 
 	n_columns = 20
