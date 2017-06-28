30636a85af3980e2099e825bfbdde83deea27410
Baeta18 <rafael.baeta@gmail.com>
2017-06-28 15:18:57 -0300

    whatevs

--- mnist-filter-save.py
+++ mnist-filter-save.py
@@ -127,12 +127,13 @@ def plotNNFilter(units,layer_number,label):
 		plt.subplot(n_rows, n_columns, i+1)
 		#plt.title('Filter ' + str(i))
 		plt.axis('off')
+		plt.subplots_adjust(hspace=0.1)
 		plt.imshow(units[0,:,:,i],interpolation="nearest")
 		#separeted_path = "/media/tensorflow/coffee/output/filters/weights_layer_" + str(layer_number) + "_label_" + str(label) + "_number_" + str(i) + ".png"
 		#plt.savefig(separeted_path,dpi = 50)
 		#plt.close()
 
-	plt.subplots_adjust(hspace=0.1)
+	
 	filter_path = "/media/tensorflow/coffee/output/filters/weights_layer_" + str(layer_number) + "_label_" + str(label) + ".png"
 	print("Saving image at: " + filter_path)
 	plt.savefig(filter_path)
