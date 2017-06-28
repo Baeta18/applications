441b098ff9c56eb593985022897f63d057d67920
Baeta18 <rafael.baeta@gmail.com>
2017-06-28 12:16:49 -0300

    whatevs

--- mnist-filter-save.py
+++ mnist-filter-save.py
@@ -295,7 +295,6 @@ sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 saver.restore(sess, model_path)
 
 normalizeImages(batch_x, mean_full, std_full)
-batch_x = np.reshape(batch_x,(-1,n_input))
 testAccuracy = sess.run(acc_mean, feed_dict={x:batch_x,y:batch_y, keep_prob:1.,is_training: False})
 print(testAccuracy)
 
