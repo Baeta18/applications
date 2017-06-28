0a02f2c6d3c1f259dc245ca85376c7d3ca7a7bf4
Baeta18 <rafael.baeta@gmail.com>
2017-06-28 10:16:14 -0300

    whatevs

--- mnist-filter-save.py
+++ mnist-filter-save.py
@@ -22,7 +22,7 @@ def plotNNFilter(units):
     for i in range(filters):
         plt.subplot(n_rows, n_columns, i+1)
         plt.title('Filter ' + str(i))
-        plt.plot(units[0,:,:,i], interpolation="nearest")
+        plt.plot(units[0,:,:,i])
        
 print("Load dataset")
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
