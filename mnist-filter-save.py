67fb86c9b998627ede8c14183f902d9681b2b188
Baeta18 <rafael.baeta@gmail.com>
2017-06-28 12:22:55 -0300

    whatevs

--- mnist-filter-save.py
+++ mnist-filter-save.py
@@ -222,8 +222,8 @@ print(trainMask.shape)
 
 
 
-patch = img[680:721,480:521,:]
-label = retrieveClass(mask[700][500])
+patch = img[180:221,480:521,:]
+label = retrieveClass(mask[200][500])
 
 #patch = img[470:511,130:171,:]
 #label = 1
