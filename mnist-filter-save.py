bacf67c7d4db6d8e7503e0014da07e7f55b66888
Baeta18 <rafael.baeta@gmail.com>
2017-06-28 10:51:36 -0300

    whatevs

--- mnist-filter-save.py
+++ mnist-filter-save.py
@@ -10,6 +10,38 @@ import math
 
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.1)
 
+def manipulateBorderArray(data, cropSize):
+	mask = int(cropSize/2)
+
+	h,w = len(data), len(data[0])
+	crop_left = data[0:h,0:cropSize]
+	crop_right = data[0:h,w-cropSize:w,:]
+	crop_top = data[0:cropSize,0:w,:]
+	crop_bottom = data[h-cropSize:h,0:w,:]
+
+	mirror_left = np.fliplr(crop_left)
+	mirror_right = np.fliplr(crop_right)
+	flipped_top = np.flipud(crop_top)
+	flipped_bottom = np.flipud(crop_bottom)
+
+	h_new,w_new = h+mask*2, w+mask*2
+	data_border = np.zeros((h_new, w_new, len(data[0][0])))
+	#print data_border.shape
+	data_border[mask:h+mask,mask:w+mask,:] = data
+
+	data_border[mask:h+mask, 0:mask, :] = mirror_left[:, mask+1:, :]
+	data_border[mask:h+mask, w_new-mask:w_new ,:] = mirror_right[:,0:mask,:]
+	data_border[0:mask, mask:w+mask, :] = flipped_top[mask+1:, : ,:]
+	data_border[h+mask:h+mask+mask, mask:w+mask, :] = flipped_bottom[0:mask, : ,:]
+
+	data_border[0:mask, 0:mask, :] = flipped_top[mask+1:, 0:mask ,:]
+	data_border[0:mask, w+mask:w+mask+mask, :] = flipped_top[mask+1:, w-mask:w ,:]
+	data_border[h+mask:h+mask+mask, 0:mask, :] = flipped_bottom[0:mask, 0:mask ,:]
+	data_border[h+mask:h+mask+mask, w+mask:w+mask+mask, :] = flipped_bottom[0:mask, w-mask:w ,:]
+
+	#scipy.misc.imsave('C:\\Users\\Keiller\\Desktop\\outfile.jpg', data_border)
+	return data_border
+
 def read_image_P2_int16(filename):
 	fp = open(filename,"r")
 
