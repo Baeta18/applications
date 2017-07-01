159678a95b38812b4bbbddeab46be54434e6b189
Baeta18 <rafael.baeta@gmail.com>
2017-07-01 17:01:13 -0300

    whatevs

--- mini_batch_run_9.sh
+++ mini_batch_run_9.sh
@@ -23,5 +23,5 @@
 #9
 #python /media/tensorflow/coffee/applications/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_7,9_5,8_5,7_5,7_7,9_7 7_7 8_6,9_6,7_6 25 0.001 0.005 250 0 5 0 9 0 0 3
 #10
-#python /media/tensorflow/coffee/applications/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 7_5,8_7,9_6,8_5,8_6,9_5 7_7 9_7,7_7,7_6 25 0.001 0.005 250 0 5 0 10 0 0 3
+python /media/tensorflow/coffee/applications/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 7_5,8_7,9_6,8_5,8_6,9_5 7_7 9_7,7_7,7_6 25 0.001 0.005 250 0 5 0 10 0 0 3
 
