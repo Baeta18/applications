409c2c841295b9f5cf08d983646a3d1dd6213ecc
Baeta18 <rafael.baeta@gmail.com>
2017-07-01 12:52:07 -0300

    whatevs

--- maps-fusion.sh
+++ maps-fusion.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-
-
 #'dataPath', 'outputPath(for model, images, etc)','instance' , 'fusion-instances','cropSize','fusion-type'
-python /media/tensorflow/coffee/applications/maps-fusion.py /media/tensorflow/coffee/output/results/probability/ /media/tensorflow/coffee/output/results/maps-fusion 
-/ 4 9_5,8_6,8_7 41 1
+python /media/tensorflow/coffee/applications/maps-fusion.py /media/tensorflow/coffee/output/results/probability/ /media/tensorflow/coffee/output/results/maps-fusion/ 4 9_5,8_6,8_7 41 1
