788bb3a83a5e800903573610f5b2eb58e1af7bf7
Baeta18 <rafael.baeta@gmail.com>
2017-09-05 17:22:35 -0300

    whatevs

--- mini_batch_run_65.sh
+++ mini_batch_run_65.sh
@@ -3,7 +3,7 @@
 #dataPath, outputPath(for model, images, etc), trainInstances,fsa validationInstances,testInstances,cropSize,learningRate,weightDecay,batchSize,nIter,epochs,trainModel,instance,useMinibatch,useValidation,blocks,useBalance,gpuUse
 
 #1
-python /media/tensorflow/coffee/applications/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_7,9_7,7_7,9_6,8_6,8_5 7_7 7_6,7_5,9_5 65 0.001 0.005 100 0 10 1 2 0 0 5 0 1
+python /media/tensorflow/coffee/applications/subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_7,9_7,7_7,9_6,8_6,8_5 7_7 7_6,7_5,9_5 65 0.001 0.005 100 0 10 1 2 0 0 5 0 1
 #2
 python /media/tensorflow/coffee/applications/mini_batch_subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_7,9_7,7_7,9_6,8_6,8_5 7_7 7_6,7_5,9_5 25 0.001 0.005 100 0 5 1 2 0 0 5 0 1
 #3
