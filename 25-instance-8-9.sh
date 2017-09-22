3af5cac5ec152b9072df52aa97acb632a0e58f2d
Baeta18 <rafael.baeta@gmail.com>
2017-09-22 09:15:06 -0300

    whatevs

--- 25-instance-8-9.sh
+++ 25-instance-8-9.sh
@@ -3,8 +3,8 @@
 #dataPath, outputPath(for model, images, etc), trainInstances, validationInstances,testInstances,cropSize,learningRate,weightDecay,batchSize,nIter,epochs,trainModel,instance,useMinibatch,useValidation,blocks
 
 #8
-python /media/tensorflow/coffee/applications/subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 9_7,7_7,8_5,7_6,7_5,9_5 7_7 8_6,8_7,9_6 25 0.001 0.005 350 0 10 1 8 0 0 3 0 0.30
+python /media/tensorflow/coffee/applications/subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 9_7,7_7,8_5,7_6,7_5,9_5 7_7 8_6,8_7,9_6 25 0.001 0.005 350 0 10 1 8 1 0 3 0 0.30
 #9
-python /media/tensorflow/coffee/applications/subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_7,9_5,8_5,7_5,7_7,9_7 7_7 8_6,9_6,7_6 25 0.001 0.005 350 0 10 1 9 0 0 3 0 0.30
+python /media/tensorflow/coffee/applications/subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_7,9_5,8_5,7_5,7_7,9_7 7_7 8_6,9_6,7_6 25 0.001 0.005 350 0 10 1 9 1 0 3 0 0.30
 
 
