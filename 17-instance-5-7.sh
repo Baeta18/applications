3653003f4ec3e3b7e31ba72edb82ea7f4211e585
Baeta18 <rafael.baeta@gmail.com>
2017-09-22 09:23:00 -0300

    whatevs

---
+++ 17-instance-5-7.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#5
+python /media/tensorflow/coffee/applications/subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_7,8_5,9_5,7_5,7_7,8_6 7_7 7_6,9_6,9_7 57 0.001 0.005 100 0 3 0 5 0 0 6 0 1
+#6
+python /media/tensorflow/coffee/applications/subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 7_5,8_7,9_5,8_5,9_6,7_7 7_7 8_6,7_6,9_7 57 0.001 0.005 100 0 3 0 6 0 0 6 0 1
+#7
+python /media/tensorflow/coffee/applications/subimages_6x3.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ 8_6,8_7,9_7,8_5,7_6,7_5 7_7 9_5,7_7,9_6 57 0.001 0.005 100 0 3 0 7 0 0 6 0 1
