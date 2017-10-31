e7e25b294e195fa71bbd75cb66caa7dfbf9930b6
Rafael Baeta <rafael.baeta@gmail.com>
2017-10-31 18:52:53 -0200

    whatevs

--- grss2014.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#dataPath, outputPath(for model, images, etc), trainInstances, validationInstances,testInstances,cropSize,learningRate,weightDecay,batchSize,nIter,epochs,trainModel,useMinibatch,useValidation,blocks,gpuUse
-
-python /media/tensorflow/coffee/applications/grss2014.py /media/tensorflow/coffee/dataset/ /media/tensorflow/coffee/output/ train validation teste 65 0.001 0.005 100 0 10 1 0 5 1
-
--- teste.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import random
-a = np.arange(100)
-b = 10
-extractRandom = np.asarray(random.sample(xrange(len(a)), b))
-#print(a)
-print(extractRandom)
-
-	
